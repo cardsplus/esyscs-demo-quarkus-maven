510478f6592c9523d244086a36376401a8e7b1cd
robert.bruckbauer <robert.bruckbauer@e-mundo.de>
2024-08-13 18:09:21 +0200

    quarkus ext add io.quarkiverse.openapi.generator:quarkus-openapi-generator

--- src/main/java/esy/demo/app/GreetingResource.java
+++ src/main/java/esy/demo/app/GreetingResource.java
@@ -1,16 +1,31 @@
 package esy.demo.app;
 
+import esy.demo.jsonplaceholder.api.PostsApi;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import lombok.RequiredArgsConstructor;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import java.time.LocalDate;
 
 @Path("/hello")
 public class GreetingResource {
 
+    private final PostsApi postsApi;
+
+    @Inject
+    public GreetingResource(@RestClient final PostsApi postsApi) {
+        this.postsApi = postsApi;
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        final var day = LocalDate.now().getDayOfMonth();
+        final var post = postsApi.getPost((long) day);
+        return String.format("%d: %s", day, post.getTitle());
     }
 }
--- src/test/java/esy/demo/app/GreetingResourceTest.java
+++ src/test/java/esy/demo/app/GreetingResourceTest.java
@@ -1,20 +1,26 @@
 package esy.demo.app;
 
+import esy.demo.jsonplaceholder.api.PostsApi;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectSpy;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
 
 @QuarkusTest
 class GreetingResourceTest {
-    @Test
-    void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
-    }
 
+    @Test
+    void hello() {
+        final var day = LocalDate.now().getDayOfMonth();
+        given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(startsWith(day + ": "));
+    }
 }
