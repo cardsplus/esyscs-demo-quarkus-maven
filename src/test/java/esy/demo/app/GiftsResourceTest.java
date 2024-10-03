292f271d5cf1b6a95c544c8d5bc7516ed3fbe1ac
robert.bruckbauer <robert.bruckbauer@e-mundo.de>
2024-10-03 11:20:11 +0200

    quarkus.http.test-port=0

--- src/test/java/esy/demo/app/GiftsResourceTest.java
+++ src/test/java/esy/demo/app/GiftsResourceTest.java
@@ -3,17 +3,13 @@ package esy.demo.app;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
-
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.startsWith;
 
 @QuarkusTest
 class GiftsResourceTest {
 
     @Test
     void gifts() {
-        final var day = LocalDate.now().getDayOfMonth();
         given()
                 .when().get("/api/gifts")
                 .then()
